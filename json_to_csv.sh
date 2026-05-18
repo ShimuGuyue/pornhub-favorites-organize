58b83fcc7e79bb5079ead7bccc52edf322b3b910
ShimuGuyue <shimuguyue513@qq.com>
2026-05-18 22:03:22 +0800

    Fix 解决 CSV 中 id 中纯数字文本被工具当作数字格式的问题

--- json_to_csv.sh
+++ json_to_csv.sh
@@ -5,7 +5,6 @@ echo "正在运行 json_to_csv.sh..."
 echo
 
 # 1. 检查配置文件
-
 echo "1. 正在加载个人信息配置文件..."
 
 CONF_FILE="./personal_info.conf"
@@ -30,18 +29,42 @@ echo "正在将 JSON 文件转换为 CSV 文件..."
 
 if [[ ! -f "$VEDIO_INFO_CSV" ]]; then
     echo "未检测到历史 CSV 文件，正在全新创建..."
-    printf '\xEF\xBB\xBF"id","作者","标题","时长","标签"\n' > "$VEDIO_INFO_CSV"
-    jq -r '.[] | [.id, .["作者"], .["标题"], .["时长"]] | @csv' "$VEDIO_INFO_JSON" >> "$VEDIO_INFO_CSV"
+
+    printf '\xEF\xBB\xBFid,作者,标题,时长,标签\n' > "$VEDIO_INFO_CSV"
+
+    jq -r '.[] |
+        "[\(.id)]," +
+        "\(."作者" | split(",") | join("，"))," +
+        "\(."标题" | split(",") | join("，"))," +
+        "\(."时长"),"' \
+        "$VEDIO_INFO_JSON" | while LC_ALL=C read -r row
+    do
+        id=$(echo "$row" | cut -d',' -f1 | tr -d '"[]')
+        if [[ -n "$id" ]]; then
+            echo "写入: [ID: $id]"
+            echo "$row" >> "$VEDIO_INFO_CSV"
+        fi
+    done
 else
     echo "检测到已有历史 CSV 文件，正在增量写入..."
-    jq -r '.[] | [.id, .["作者"], .["标题"], .["时长"]] | @csv' "$VEDIO_INFO_JSON" | while LC_ALL=C read -r row
+
+    jq -r '.[] |
+        "[\(.id)]," +
+        "\(."作者" | split(",") | join("，"))," +
+        "\(."标题" | split(",") | join("，"))," +
+        "\(."时长"),"' \
+        "$VEDIO_INFO_JSON" | while LC_ALL=C read -r row
     do
-        id=$(echo "$row" | cut -d',' -f1 | tr -d '"')
-        if grep -q -F "$id" "$VEDIO_INFO_CSV"; then
-            echo "跳过: [ID: $id] 相关数据已存在"
-        else
-            echo "添加: [ID: $id] 是新数据，已追加至末尾"
-            echo "$row" >> "$VEDIO_INFO_CSV"
+        id=$(echo "$row" | cut -d',' -f1 | tr -d '"[]')
+        if [[ -n "$id" ]]; then
+            id_match="^\"?\[${id}\]"
+
+            if grep -E -q "$id_match" "$VEDIO_INFO_CSV"; then
+                echo "跳过: [ID: $id] 相关数据已存在"
+            else
+                echo "写入: [ID: $id] 是新数据，已追加至末尾"
+                echo "$row" >> "$VEDIO_INFO_CSV"
+            fi
         fi
     done
 fi
